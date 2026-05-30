90d75347e48151f442812cb922b30d5c231c1e66
Yehor Titov <tegor901@gmail.com>
2026-05-30 13:19:58 +0200

    add custom exceptions for login/register scenarios

---
+++ backend/src/ChatApp.Application/Exceptions/InvalidCredentialsException.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatApp.Application.Exceptions;
+
+public class InvalidCredentialsException : Exception
+{
+    public InvalidCredentialsException()
+        : base("Invalid credentials")
+    {
+    }
+}
---
+++ backend/src/ChatApp.Application/Exceptions/UserAlreadyExistsException.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatApp.Application.Exceptions;
+
+public class UserAlreadyExistsException : Exception
+{
+    public UserAlreadyExistsException()
+        : base("User with this email already exists")
+    {
+    }
+}
