1f96e7dfd89b299fbeccad6dc0b4ddfe7c7b79d2
Yehor Titov <tegor901@gmail.com>
2026-05-31 13:19:55 +0200

    add validators for register/login with FluentValidation

---
+++ backend/src/ChatApp.Application/Validators/LoginRequestDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using ChatApp.Application.DTOs;
+using FluentValidation;
+
+namespace ChatApp.Application.Validators;
+
+public class LoginRequestDtoValidator
+    : AbstractValidator<LoginRequestDto>
+{
+    public LoginRequestDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty();
+    }
+}
---
+++ backend/src/ChatApp.Application/Validators/RegisterRequestDtoValidator.cs
@@ -0,0 +1,33 @@
+﻿using ChatApp.Application.DTOs;
+using FluentValidation;
+
+namespace ChatApp.Application.Validators;
+
+public class RegisterRequestDtoValidator
+    : AbstractValidator<RegisterRequestDto>
+{
+    public RegisterRequestDtoValidator()
+    {
+        RuleFor(x => x.Username)
+            .NotEmpty()
+            .MinimumLength(3)
+            .MaximumLength(30)
+            .Matches("^[a-zA-Z0-9_]+$")
+            .WithMessage(
+                "Username may contain only letters, numbers and underscore");
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(8)
+            .Matches("[A-Z]")
+            .WithMessage("Password must contain at least one uppercase letter")
+            .Matches("[a-z]")
+            .WithMessage("Password must contain at least one lowercase letter")
+            .Matches("[0-9]")
+            .WithMessage("Password must contain at least one digit");
+    }
+}
