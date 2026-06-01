c55313e92646f655d017c4bb1ac4b9fed12fd62d
Yehor Titov <tegor901@gmail.com>
2026-06-01 14:16:46 +0200

    add workspace, workspace member entities with workspace role enum

---
+++ backend/src/ChatApp.Domain/Entities/WorkspaceMember.cs
@@ -0,0 +1,16 @@
+﻿using ChatApp.Domain.Enums;
+
+namespace ChatApp.Domain.Entities;
+
+public class WorkspaceMember
+{
+    public Guid WorkspaceId { get; set; }
+    public Workspace Workspace { get; set; } = null!;
+
+    public Guid UserId { get; set; }
+    public User User { get; set; } = null!;
+
+    public WorkspaceRole Role { get; set; }
+
+    public DateTime JoinedAt { get; set; } = DateTime.UtcNow;
+}
