836b0439e8d7cdac0a29b90b8a2434ddd5c82f1d
Yehor Titov <tegor901@gmail.com>
2026-06-01 14:18:31 +0200

    add database configurations for workspace and workspace member entities

---
+++ backend/src/ChatApp.Infrastructure/Persistence/Configurations/WorkspaceConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using ChatApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChatApp.Infrastructure.Persistence.Configurations;
+
+public class WorkspaceConfiguration
+    : IEntityTypeConfiguration<Workspace>
+{
+    public void Configure(
+        EntityTypeBuilder<Workspace> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(500);
+    }
+}
---
+++ backend/src/ChatApp.Infrastructure/Persistence/Configurations/WorkspaceMemberConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using ChatApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChatApp.Infrastructure.Persistence.Configurations;
+
+public class WorkspaceMemberConfiguration
+    : IEntityTypeConfiguration<WorkspaceMember>
+{
+    public void Configure(
+        EntityTypeBuilder<WorkspaceMember> builder)
+    {
+        builder.HasKey(x =>
+            new
+            {
+                x.WorkspaceId,
+                x.UserId
+            });
+
+        builder
+            .HasOne(x => x.Workspace)
+            .WithMany(x => x.Members)
+            .HasForeignKey(x => x.WorkspaceId);
+
+        builder
+            .HasOne(x => x.User)
+            .WithMany(x => x.WorkspaceMemberships)
+            .HasForeignKey(x => x.UserId);
+    }
+}
