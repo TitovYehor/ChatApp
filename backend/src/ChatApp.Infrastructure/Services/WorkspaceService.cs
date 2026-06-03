6f6fb10432953d517ef284bb6704ce7d737b36bb
Yehor Titov <tegor901@gmail.com>
2026-06-03 12:18:51 +0200

    implement initial workspace service

---
+++ backend/src/ChatApp.Infrastructure/Services/WorkspaceService.cs
@@ -0,0 +1,45 @@
+﻿using ChatApp.Application.DTOs.Workspaces;
+using ChatApp.Application.Interfaces;
+using ChatApp.Application.Mappings;
+using ChatApp.Domain.Entities;
+using ChatApp.Domain.Enums;
+using ChatApp.Infrastructure.Persistence;
+
+namespace ChatApp.Infrastructure.Services;
+
+public class WorkspaceService : IWorkspaceService
+{
+    private readonly AppDbContext _dbContext;
+
+    public WorkspaceService(
+        AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<WorkspaceResponseDto> CreateAsync(
+        Guid userId,
+        CreateWorkspaceRequestDto request)
+    {
+        var workspace = new Workspace
+        {
+            Name = request.Name,
+            Description = request.Description
+        };
+
+        var membership = new WorkspaceMember
+        {
+            Workspace = workspace,
+            UserId = userId,
+            Role = WorkspaceRole.Owner
+        };
+
+        _dbContext.Workspaces.Add(workspace);
+
+        _dbContext.WorkspaceMembers.Add(membership);
+
+        await _dbContext.SaveChangesAsync();
+
+        return workspace.ToDto();
+    }
+}
