558c64f0331c464ed7f8b42067a4cedba545873d
Yehor Titov <tegor901@gmail.com>
2026-06-03 12:18:04 +0200

    add mapping extension for workspace

---
+++ backend/src/ChatApp.Application/Mappings/WorkspaceMappings.cs
@@ -0,0 +1,19 @@
+﻿using ChatApp.Application.DTOs.Workspaces;
+using ChatApp.Domain.Entities;
+
+namespace ChatApp.Application.Mappings;
+
+public static class WorkspaceMappings
+{
+    public static WorkspaceResponseDto ToDto(
+        this Workspace workspace)
+    {
+        return new WorkspaceResponseDto
+        {
+            Id = workspace.Id,
+            Name = workspace.Name,
+            Description = workspace.Description,
+            CreatedAt = workspace.CreatedAt
+        };
+    }
+}
