68366b7d2f57c82e2402480166b437a6a8adc59b
Yehor Titov <tegor901@gmail.com>
2026-06-06 12:40:24 +0200

    add Channel entity database configuration

---
+++ backend/src/ChatApp.Infrastructure/Persistence/Configurations/ChannelConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using ChatApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChatApp.Infrastructure.Persistence.Configurations;
+
+public class ChannelConfiguration
+    : IEntityTypeConfiguration<Channel>
+{
+    public void Configure(
+        EntityTypeBuilder<Channel> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder
+            .HasOne(x => x.Workspace)
+            .WithMany(x => x.Channels)
+            .HasForeignKey(x => x.WorkspaceId);
+
+        builder
+            .HasIndex(x => new
+            {
+                x.WorkspaceId,
+                x.Name
+            })
+            .IsUnique();
+    }
+}
