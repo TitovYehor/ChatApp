ee80e9662c4149ef5882442b456dfacbf7b74553
Yehor Titov <tegor901@gmail.com>
2026-06-07 12:45:16 +0200

    add mapping extension for channel Entity - DTO

---
+++ backend/src/ChatApp.Application/Mappings/ChannelMappings.cs
@@ -0,0 +1,20 @@
+﻿using ChatApp.Application.DTOs.Channels;
+using ChatApp.Domain.Entities;
+
+namespace ChatApp.Application.Mappings;
+
+public static class ChannelMappings
+{
+    public static ChannelResponseDto ToDto(
+        this Channel channel)
+    {
+        return new ChannelResponseDto
+        {
+            Id = channel.Id,
+            WorkspaceId = channel.WorkspaceId,
+            Name = channel.Name,
+            Type = (int)channel.Type,
+            CreatedAt = channel.CreatedAt
+        };
+    }
+}
