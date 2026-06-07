2c71b2bf760a232ceba4e20b694cf27b1eeb30b5
Yehor Titov <tegor901@gmail.com>
2026-06-07 12:45:35 +0200

    add validator for channel creation requests

---
+++ backend/src/ChatApp.Application/Validators/CreateChannelRequestDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using ChatApp.Application.DTOs.Channels;
+using FluentValidation;
+
+namespace ChatApp.Application.Validators;
+
+public class CreateChannelRequestDtoValidator
+    : AbstractValidator<CreateChannelRequestDto>
+{
+    public CreateChannelRequestDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(100);
+    }
+}
