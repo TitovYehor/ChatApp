2e88f40bc6f7a2e024283de470a7082e573941c7
Yehor Titov <tegor901@gmail.com>
2026-06-07 12:45:58 +0200

    add initial interface for channel service

---
+++ backend/src/ChatApp.Application/Interfaces/IChannelService.cs
@@ -0,0 +1,11 @@
+﻿using ChatApp.Application.DTOs.Channels;
+
+namespace ChatApp.Application.Interfaces;
+
+public interface IChannelService
+{
+    Task<ChannelResponseDto> CreateAsync(
+        Guid workspaceId,
+        Guid userId,
+        CreateChannelRequestDto request);
+}
