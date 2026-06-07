78a27575c6f7dd78dc3875f08aa32e928fbdb6ee
Yehor Titov <tegor901@gmail.com>
2026-06-07 12:46:34 +0200

    add channel service implementing initial channel service interface

---
+++ backend/src/ChatApp.Infrastructure/Services/ChannelService.cs
@@ -0,0 +1,50 @@
+﻿using ChatApp.Application.DTOs.Channels;
+using ChatApp.Application.Exceptions;
+using ChatApp.Application.Interfaces;
+using ChatApp.Application.Mappings;
+using ChatApp.Domain.Entities;
+using ChatApp.Domain.Enums;
+using ChatApp.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApp.Infrastructure.Services;
+
+public class ChannelService : IChannelService
+{
+    private readonly AppDbContext _dbContext;
+
+    public ChannelService(
+        AppDbContext dbContext)
+    {
+        this._dbContext = dbContext;
+    }
+
+    public async Task<ChannelResponseDto> CreateAsync(
+        Guid workspaceId,
+        Guid userId,
+        CreateChannelRequestDto request)
+    {
+        var isMember = await _dbContext.WorkspaceMembers
+            .AnyAsync(x =>
+                x.WorkspaceId == workspaceId &&
+                x.UserId == userId);
+
+        if (!isMember)
+        {
+            throw new ForbiddenException("User is not a member of the workspace");
+        }
+
+        var channel = new Channel
+        {
+            WorkspaceId = workspaceId,
+            Name = request.Name,
+            Type = ChannelType.Text
+        };
+
+        _dbContext.Channels.Add(channel);
+
+        await _dbContext.SaveChangesAsync();
+
+        return channel.ToDto();
+    }
+}
