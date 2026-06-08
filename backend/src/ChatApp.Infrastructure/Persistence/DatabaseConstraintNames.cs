2daa843c0d15323ac1fd1f5182b5da4987b041d8
Yehor Titov <tegor901@gmail.com>
2026-06-08 14:14:34 +0200

    add centralized class for unique constraint names

---
+++ backend/src/ChatApp.Infrastructure/Persistence/DatabaseConstraintNames.cs
@@ -0,0 +1,7 @@
+﻿namespace ChatApp.Infrastructure.Persistence;
+
+public static class DatabaseConstraintNames
+{
+    public const string UniqueChannelNamePerWorkspace =
+        "IX_Channels_WorkspaceId_Name";
+}
