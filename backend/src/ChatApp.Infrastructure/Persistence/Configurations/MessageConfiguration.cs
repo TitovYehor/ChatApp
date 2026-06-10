3e9fa4f31ddf99af11279f0431496ffb35d25eac
Yehor Titov <tegor901@gmail.com>
2026-06-10 12:14:27 +0200

    add database configurations to Message table

---
+++ backend/src/ChatApp.Infrastructure/Persistence/Configurations/MessageConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using ChatApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChatApp.Infrastructure.Persistence.Configurations;
+
+public class MessageConfiguration
+    : IEntityTypeConfiguration<Message>
+{
+    public void Configure(
+        EntityTypeBuilder<Message> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Content)
+            .IsRequired()
+            .HasMaxLength(2000);
+
+        builder.HasOne(x => x.Channel)
+            .WithMany(x => x.Messages)
+            .HasForeignKey(x => x.ChannelId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.User)
+            .WithMany(x => x.Messages)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(x => new
+        {
+            x.ChannelId,
+            x.CreatedAt
+        });
+    }
+}
