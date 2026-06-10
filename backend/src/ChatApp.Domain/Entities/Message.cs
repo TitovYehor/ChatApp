a0fdb5125eee927a41ed5dcf6d382f6e888054e8
Yehor Titov <tegor901@gmail.com>
2026-06-10 12:13:20 +0200

    add Message entity

---
+++ backend/src/ChatApp.Domain/Entities/Message.cs
@@ -0,0 +1,20 @@
+﻿namespace ChatApp.Domain.Entities;
+
+public class Message
+{
+    public Guid Id { get; set; }
+
+    public Guid ChannelId { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string Content { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public Channel Channel { get; set; } = null!;
+
+    public User User { get; set; } = null!;
+}
