c4a57bbb93c70bd900ef63bab3a7b07f0e4c94d3
Yehor Titov <tegor901@gmail.com>
2026-06-11 12:09:47 +0200

    add initial mapping for messages from Entity to DTO

---
+++ backend/src/ChatApp.Application/Mappings/MessageMappings.cs
@@ -0,0 +1,22 @@
+﻿using ChatApp.Application.DTOs.Messages;
+using ChatApp.Domain.Entities;
+
+namespace ChatApp.Application.Mappings;
+
+public static class MessageMappings
+{
+    public static MessageResponseDto ToDto(
+        this Message message)
+    {
+        return new MessageResponseDto
+        {
+            Id = message.Id,
+            ChannelId = message.ChannelId,
+            UserId = message.UserId,
+            Username = message.User.Username,
+            Content = message.Content,
+            CreatedAt = message.CreatedAt,
+            UpdatedAt = message.UpdatedAt
+        };
+    }
+}
