639a74c33ee135edaea18c267770ba82b5455ff2
Yehor Titov <tegor901@gmail.com>
2026-06-12 12:15:14 +0200

    add PagedResult as common returning form of paginated results

---
+++ backend/src/ChatApp.Application/DTOs/Common/PagedResult.cs
@@ -0,0 +1,13 @@
+﻿namespace ChatApp.Application.DTOs.Common;
+
+public class PagedResult<T>
+{
+    public IReadOnlyCollection<T> Items { get; set; }
+        = new List<T>();
+
+    public int PageNumber { get; set; }
+
+    public int PageSize { get; set; }
+
+    public int TotalCount { get; set; }
+}
