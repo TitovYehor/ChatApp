44d82e01fa74456956b1da60031eabbae025816a
Yehor Titov <tegor901@gmail.com>
2026-06-12 12:13:43 +0200

    add MessageQueryDto with it's own validator for searching requests

---
+++ backend/src/ChatApp.Application/DTOs/Messages/MessageQueryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatApp.Application.DTOs.Messages;
+
+public class MessageQueryDto
+{
+    public int PageNumber { get; set; } = 1;
+
+    public int PageSize { get; set; } = 50;
+}
---
+++ backend/src/ChatApp.Application/Validators/MessageQueryDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using ChatApp.Application.DTOs.Messages;
+using FluentValidation;
+
+namespace ChatApp.Application.Validators;
+
+public class MessageQueryDtoValidator
+    : AbstractValidator<MessageQueryDto>
+{
+    public MessageQueryDtoValidator()
+    {
+        RuleFor(x => x.PageNumber)
+            .GreaterThan(0);
+
+        RuleFor(x => x.PageSize)
+            .InclusiveBetween(1, 100);
+    }
+}
