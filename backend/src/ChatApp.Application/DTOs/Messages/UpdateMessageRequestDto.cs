13d4e4331b8214a1774dd6ac10038112653827e3
Yehor Titov <tegor901@gmail.com>
2026-06-13 13:14:56 +0200

    add UpdateMessageRequestDto model for updating messages add validator for message updating requests

---
+++ backend/src/ChatApp.Application/DTOs/Messages/UpdateMessageRequestDto.cs
@@ -0,0 +1,6 @@
+﻿namespace ChatApp.Application.DTOs.Messages;
+
+public class UpdateMessageRequestDto
+{
+    public string Content { get; set; } = string.Empty;
+}
---
+++ backend/src/ChatApp.Application/Validators/UpdateMessageRequestDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using ChatApp.Application.DTOs.Messages;
+using FluentValidation;
+
+namespace ChatApp.Application.Validators;
+
+public class UpdateMessageRequestDtoValidator
+    : AbstractValidator<UpdateMessageRequestDto>
+{
+    public UpdateMessageRequestDtoValidator()
+    {
+        RuleFor(x => x.Content)
+            .NotEmpty()
+            .MaximumLength(2000);
+    }
+}
