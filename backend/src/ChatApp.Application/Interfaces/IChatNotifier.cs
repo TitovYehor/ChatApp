639951671871940ce2f2d97bb6cf79a33cee0ac4
Yehor Titov <tegor901@gmail.com>
2026-06-15 13:31:01 +0200

    add interface IChatNotifier for use with SignalR

---
+++ backend/src/ChatApp.Application/Interfaces/IChatNotifier.cs
@@ -0,0 +1,18 @@
+﻿using ChatApp.Application.DTOs.Messages;
+
+namespace ChatApp.Application.Interfaces;
+
+public interface IChatNotifier
+{
+    Task MessageCreatedAsync(
+        Guid channelId,
+        MessageResponseDto message);
+
+    Task MessageUpdatedAsync(
+        Guid channelId,
+        MessageResponseDto message);
+
+    Task MessageDeletedAsync(
+        Guid channelId,
+        Guid messageId);
+}
