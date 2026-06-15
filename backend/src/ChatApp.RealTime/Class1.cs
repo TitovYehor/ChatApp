3844c3deb8c573a21e8a6c773fc51ae9c5997aeb
Yehor Titov <tegor901@gmail.com>
2026-06-15 13:51:12 +0200

    add initial version of ChatHub

--- backend/src/ChatApp.RealTime/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ChatApp.RealTime;
-
-public class Class1
-{
-
-}
---
+++ backend/src/ChatApp.RealTime/Hubs/ChatHub.cs
@@ -0,0 +1,49 @@
+﻿using ChatApp.Application.Realtime;
+using ChatApp.Application.Realtime.Requests;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.RealTime.Hubs;
+
+[Authorize]
+public sealed class ChatHub : Hub
+{
+    public override async Task OnConnectedAsync()
+    {
+        Console.WriteLine($"Connected: {Context.ConnectionId}");
+
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(
+        Exception? exception)
+    {
+        Console.WriteLine($"Disconnected: {Context.ConnectionId}");
+
+        await base.OnDisconnectedAsync(exception);
+    }
+
+    public async Task JoinChannel(
+        JoinChannelRequest request)
+    {
+        Console.WriteLine(
+            $"Connection {Context.ConnectionId} joined channel {request.ChannelId}");
+
+        await Groups.AddToGroupAsync(
+            Context.ConnectionId,
+            SignalRGroups.Channel(
+                request.ChannelId));
+    }
+
+    public async Task LeaveChannel(
+        LeaveChannelRequest request)
+    {
+        Console.WriteLine(
+            $"Connection {Context.ConnectionId} left channel {request.ChannelId}");
+
+        await Groups.RemoveFromGroupAsync(
+            Context.ConnectionId,
+            SignalRGroups.Channel(
+                request.ChannelId));
+    }
+}
