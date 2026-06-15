95a2b86a1f21ac037181049f839eae8e00a50d51
Yehor Titov <tegor901@gmail.com>
2026-06-15 13:53:19 +0200

    add extension of Dependency Injection to Realtime project for registering all related to SignalR services and technologies

---
+++ backend/src/ChatApp.RealTime/Extensions/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ChatApp.RealTime.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddRealTime(
+        this IServiceCollection services)
+    {
+        services.AddSignalR();
+
+        return services;
+    }
+}
