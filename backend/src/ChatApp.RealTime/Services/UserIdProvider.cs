86fcf5f5b43eccb791cec6a035bce3fdc323307b
Yehor Titov <tegor901@gmail.com>
2026-06-16 11:34:09 +0200

    add service UserIdProvider for retrieving current user Id

---
+++ backend/src/ChatApp.RealTime/Services/UserIdProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.RealTime.Services;
+
+public sealed class UserIdProvider
+    : IUserIdProvider
+{
+    public string? GetUserId(
+        HubConnectionContext connection)
+    {
+        return connection.User?
+            .FindFirst(
+                ClaimTypes.NameIdentifier)?
+            .Value;
+    }
+}
