ec07ff4ad5ad72080ae7d3723c0a0043f2cae1ce
Yehor Titov <tegor901@gmail.com>
2026-06-17 11:52:44 +0200

    add SignalRChatNotifier for sending noifications to hub clients

---
+++ backend/src/ChatApp.RealTime/Notifications/SignalRChatNotifier.cs
@@ -0,0 +1,54 @@
+﻿using ChatApp.Application.DTOs.Messages;
+using ChatApp.Application.Interfaces;
+using ChatApp.Application.Realtime;
+using ChatApp.RealTime.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.RealTime.Notifications;
+
+public sealed class SignalRChatNotifier : IChatNotifier
+{
+    private readonly IHubContext<ChatHub> _hubContext;
+
+    public SignalRChatNotifier(
+        IHubContext<ChatHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public async Task MessageCreatedAsync(
+        Guid channelId,
+        MessageResponseDto message)
+    {
+        await _hubContext.Clients
+            .Group(
+                SignalRGroups.Channel(channelId))
+            .SendAsync(
+                SignalREvents.MessageCreated,
+                message);
+    }
+
+    public async Task MessageUpdatedAsync(
+        Guid channelId,
+        MessageResponseDto message)
+    {
+        await _hubContext.Clients
+            .Group(
+                SignalRGroups.Channel(channelId))
+            .SendAsync(
+                SignalREvents.MessageUpdated,
+                message);
+    }
+
+    public async Task MessageDeletedAsync(
+        Guid channelId,
+        Guid messageId)
+    {
+        await _hubContext.Clients
+            .Group(
+                SignalRGroups.Channel(channelId))
+            .SendAsync(
+                SignalREvents.MessageDeleted,
+                messageId);
+    }
+}
