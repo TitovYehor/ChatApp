e4bb08a4652c4ab93126394b52b24a0b243c6b1a
Yehor Titov <tegor901@gmail.com>
2026-06-19 12:20:44 +0200

    add project SignalRTester for testing how SignalR work add SignalRTester to solution add initial testing code

---
+++ backend/tools/ChatApp.SignalRTester/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+Console.WriteLine("JWT Token:");
+
+var token = Console.ReadLine();
+
+var connection = new HubConnectionBuilder()
+    .WithUrl(
+        "https://localhost:7163/hubs/chat",
+        options =>
+        {
+            options.AccessTokenProvider =
+                () => Task.FromResult(token);
+        })
+    .WithAutomaticReconnect()
+    .Build();
+
+try
+{
+    await connection.StartAsync();
+
+    Console.WriteLine($"Connected. ConnectionId: {connection.ConnectionId}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Connection failed: {ex.Message}");
+}
+
+Console.WriteLine("Press Enter to exit");
+Console.ReadLine();
