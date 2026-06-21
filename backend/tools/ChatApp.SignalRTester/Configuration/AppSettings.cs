9d3a307f33f48251912d19592a88c940c433515d
Yehor Titov <tegor901@gmail.com>
2026-06-21 12:41:23 +0200

    add AppSettings configuration class

---
+++ backend/tools/ChatApp.SignalRTester/Configuration/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatApp.SignalRTester.Configuration;
+
+public class AppSettings
+{
+    public const string SectionName = "AppSettings";
+
+    public string ApiBaseUrl { get; init; } = string.Empty;
+}
