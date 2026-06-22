00996ffe39daf265ecf49f2aaf8b75097c2c614b
Yehor Titov <tegor901@gmail.com>
2026-06-22 13:03:53 +0200

    remove ApiClient and introduce AuthenticationApiClient with it's interface for more flexible Client structure

---
+++ backend/tools/ChatApp.SignalRTester/Clients/Authentication/AuthenticationApiClient.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Json;
+using ChatApp.Contracts.Authentication.Requests;
+using ChatApp.Contracts.Authentication.Responses;
+using ChatApp.SignalRTester.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace ChatApp.SignalRTester.Clients.Authentication;
+
+public class AuthenticationApiClient : IAuthenticationApiClient
+{
+    private readonly HttpClient _httpClient;
+
+    public AuthenticationApiClient(
+        IHttpClientFactory httpClientFactory,
+        IOptions<AppSettings> options)
+    {
+        _httpClient =
+            httpClientFactory.CreateClient();
+
+        _httpClient.BaseAddress =
+            new Uri(options.Value.ApiBaseUrl);
+    }
+
+    public async Task<AuthResponseDto?> LoginAsync(
+        LoginRequestDto request)
+    {
+        var response = await _httpClient.PostAsJsonAsync(
+            "api/auth/login",
+            request);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return null;
+        }
+
+        var authResponse = await response.Content
+            .ReadFromJsonAsync<AuthResponseDto>();
+
+        return authResponse;
+    }
+}
---
+++ backend/tools/ChatApp.SignalRTester/Clients/Authentication/IAuthenticationApiClient.cs
@@ -0,0 +1,10 @@
+﻿using ChatApp.Contracts.Authentication.Requests;
+using ChatApp.Contracts.Authentication.Responses;
+
+namespace ChatApp.SignalRTester.Clients.Authentication;
+
+public interface IAuthenticationApiClient
+{
+    Task<AuthResponseDto?> LoginAsync(
+        LoginRequestDto request);
+}
