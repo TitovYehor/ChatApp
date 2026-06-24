9d9c8309242ef6628f682453643f8ecb12bdc0f5
Yehor Titov <tegor901@gmail.com>
2026-06-24 11:58:54 +0200

    add generic ApiResult class for cleaner retrieval and storage of api responses

---
+++ backend/tools/ChatApp.SignalRTester/Models/ApiResult.cs
@@ -0,0 +1,28 @@
+﻿namespace ChatApp.SignalRTester.Models;
+
+public class ApiResult<T>
+{
+    public bool IsSuccess { get; init; }
+
+    public T? Data { get; init; }
+
+    public string? ErrorMessage { get; init; }
+
+    public static ApiResult<T> Success(T data)
+    {
+        return new ApiResult<T>
+        {
+            IsSuccess = true,
+            Data = data
+        };
+    }
+
+    public static ApiResult<T> Failure(string message)
+    {
+        return new ApiResult<T>
+        {
+            IsSuccess = false,
+            ErrorMessage = message
+        };
+    }
+}
