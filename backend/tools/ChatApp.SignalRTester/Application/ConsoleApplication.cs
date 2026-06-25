8fcc15538818234b110a302454ea2e232e125087
Yehor Titov <tegor901@gmail.com>
2026-06-25 12:16:51 +0200

    add UserSession dependency to ConsoleApplicaton use SignIn method of UserSession to store current user information instead of just displaying them

--- backend/tools/ChatApp.SignalRTester/Application/ConsoleApplication.cs
+++ backend/tools/ChatApp.SignalRTester/Application/ConsoleApplication.cs
@@ -1,5 +1,6 @@
 ﻿using ChatApp.Contracts.Authentication.Requests;
 using ChatApp.SignalRTester.Clients.Authentication;
+using ChatApp.SignalRTester.Session;
 using ChatApp.SignalRTester.UI;
 using ChatApp.SignalRTester.UI.Input;
 using ChatApp.SignalRTester.UI.Models;
@@ -14,15 +15,18 @@ public class ConsoleApplication : IConsoleApplication
 
     private readonly IConsoleInput _consoleInput;
 
+    private readonly UserSession _userSession;
 
     public ConsoleApplication(
         IConsoleMenu menu,
         IConsoleInput consoleInput,
-        IAuthenticationApiClient authenticationApiClient)
+        IAuthenticationApiClient authenticationApiClient,
+        UserSession userSession)
     {
         _menu = menu;
         _consoleInput = consoleInput;
         _authenticationApiClient = authenticationApiClient;
+        _userSession = userSession;
     }
 
     public async Task RunAsync()
@@ -75,15 +79,16 @@ public class ConsoleApplication : IConsoleApplication
 
         var response = result.Data!;
 
+        _userSession.SignIn(
+            response.Token,
+            response.Username,
+            response.Email);
+
         Console.WriteLine();
-        Console.WriteLine("Login successful!");
+        Console.WriteLine("Login successful");
         Console.WriteLine();
 
-        Console.WriteLine($"Username: {response.Username}");
-        Console.WriteLine($"Email: {response.Email}");
-        Console.WriteLine();
-
-        Console.WriteLine("Token:");
-        Console.WriteLine(response.Token);
+        Console.WriteLine($"Welcome {_userSession.Username}!");
+        Console.WriteLine($"Email: {_userSession.Email}");
     }
 }
