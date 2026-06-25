bcdf2c16b1aeaa335a21c6b3aa830f6a26939537
Yehor Titov <tegor901@gmail.com>
2026-06-25 12:11:01 +0200

    Add UserSession for storing current use information and status

---
+++ backend/tools/ChatApp.SignalRTester/Session/UserSession.cs
@@ -0,0 +1,30 @@
+﻿namespace ChatApp.SignalRTester.Session;
+
+public class UserSession
+{
+    public string? Token { get; private set; }
+
+    public string? Username { get; private set; }
+
+    public string? Email { get; private set; }
+
+    public bool IsAuthenticated =>
+        !string.IsNullOrWhiteSpace(Token);
+
+    public void SignIn(
+        string token,
+        string username,
+        string email)
+    {
+        Token = token;
+        Username = username;
+        Email = email;
+    }
+
+    public void SignOut()
+    {
+        Token = null;
+        Username = null;
+        Email = null;
+    }
+}
