5b024835579ca2c73269293ced6610a086fb29cd
Yehor Titov <tegor901@gmail.com>
2026-06-27 10:37:27 +0200

    add MenuItem class for describing each option in console menu as a class

---
+++ backend/tools/ChatApp.SignalRTester/UI/Models/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatApp.SignalRTester.UI.Models;
+
+public class MenuItem
+{
+    public int Number { get; init; }
+
+    public string Text { get; init; } = string.Empty;
+
+    public MenuOption Option { get; init; }
+
+    public bool Visible { get; init; } = true;
+}
