18cedd3859d11f017aa3452d29df9c8b2c6b22cb
Yehor Titov <tegor901@gmail.com>
2026-07-01 11:50:26 +0200

    add UpdateChannelRequestDto for channel Dto update requests add UpdateChannelRequestDtoValidator for UpdateChannelRequestDto

---
+++ backend/src/ChatApp.Application/Validators/UpdateChannelRequestDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using ChatApp.Contracts.Channels.Requests;
+using FluentValidation;
+
+namespace ChatApp.Application.Validators;
+
+public class UpdateChannelRequestDtoValidator
+    : AbstractValidator<UpdateChannelRequestDto>
+{
+    public UpdateChannelRequestDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(100);
+    }
+}
---
+++ backend/src/ChatApp.Contracts/Channels/Requests/UpdateChannelRequestDto.cs
@@ -0,0 +1,6 @@
+﻿namespace ChatApp.Contracts.Channels.Requests;
+
+public class UpdateChannelRequestDto
+{
+    public string Name { get; set; } = string.Empty;
+}
