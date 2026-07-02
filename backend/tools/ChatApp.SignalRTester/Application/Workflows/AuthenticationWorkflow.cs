b1361d8ee3f28298e3774c7f72c3d9216aaed8a0
Yehor Titov <tegor901@gmail.com>
2026-07-02 11:32:25 +0200

    add AuthenticationWorkflow to contain business logic of authentication methods instead of leaving it all directly in ConsoleApplication

---
+++ backend/tools/ChatApp.SignalRTester/Application/Workflows/AuthenticationWorkflow.cs
@@ -0,0 +1,74 @@
+﻿using ChatApp.Contracts.Authentication.Requests;
+using ChatApp.SignalRTester.Clients.Authentication;
+using ChatApp.SignalRTester.Session;
+using ChatApp.SignalRTester.UI.Input;
+using ChatApp.SignalRTester.UI.Output;
+
+namespace ChatApp.SignalRTester.Application.Workflows;
+
+public class AuthenticationWorkflow
+{
+    private readonly IAuthenticationApiClient _authenticationApiClient;
+
+    private readonly UserSession _userSession;
+
+    private readonly IConsoleInput _consoleInput;
+
+    private readonly IConsoleOutput _consoleOutput;
+
+    public AuthenticationWorkflow(
+        IAuthenticationApiClient authenticationApiClient,
+        UserSession userSession,
+        IConsoleInput consoleInput,
+        IConsoleOutput consoleOutput)
+    { 
+        _authenticationApiClient = authenticationApiClient;
+        _userSession = userSession;
+        _consoleInput = consoleInput;
+        _consoleOutput = consoleOutput;
+    }
+
+    public async Task LoginAsync()
+    {
+        _consoleOutput.WriteHeader("Login");
+
+        var email = _consoleInput.ReadRequiredString("Email");
+
+        var password = _consoleInput.ReadRequiredString("Password");
+
+        var request = new LoginRequestDto
+        {
+            Email = email,
+            Password = password
+        };
+
+        var result = await _authenticationApiClient.LoginAsync(request);
+
+        _consoleOutput.WriteSeparator();
+
+        if (!result.IsSuccess)
+        {
+            _consoleOutput.WriteError(result.ErrorMessage!);
+            return;
+        }
+
+        var response = result.Data!;
+
+        _userSession.SignIn(response);
+
+        _consoleOutput.WriteSuccess("Login successful");
+
+        _consoleOutput.WriteSeparator();
+
+        _consoleOutput.WriteInfo($"Welcome {_userSession.Username}!");
+        _consoleOutput.WriteInfo($"Email: {_userSession.Email}");
+    }
+
+    public void Logout()
+    {
+        _userSession.SignOut();
+
+        _consoleOutput.WriteSeparator();
+        _consoleOutput.WriteSuccess("Logged out successfully");
+    }
+}
