e3a864c531de53abc6a139f97001cab0f56bbece
Yehor Titov <tegor901@gmail.com>
2026-07-02 11:52:43 +0200

    add IChannelApiClient and implementation for accessing API endpoints

---
+++ backend/tools/ChatApp.SignalRTester/Clients/Channels/ChannelApiClient.cs
@@ -0,0 +1,43 @@
+﻿using ChatApp.Contracts.Channels.Requests;
+using ChatApp.Contracts.Channels.Responses;
+using ChatApp.SignalRTester.Configuration;
+using ChatApp.SignalRTester.Models;
+using ChatApp.SignalRTester.Session;
+using Microsoft.Extensions.Options;
+
+namespace ChatApp.SignalRTester.Clients.Channels;
+
+public class ChannelApiClient: ApiClientBase, IChannelApiClient
+{
+    public ChannelApiClient(
+        IHttpClientFactory factory,
+        IOptions<AppSettings> options,
+        UserSession session)
+        : base(factory.CreateClient(), session)
+    {
+        HttpClient.BaseAddress = new Uri(options.Value.ApiBaseUrl);
+    }
+
+    public Task<ApiResult<ChannelResponseDto>> CreateAsync(
+        Guid workspaceId,
+        CreateChannelRequestDto request)
+    {
+        return PostAsync<CreateChannelRequestDto, ChannelResponseDto>(
+            $"api/workspaces/{workspaceId}/channels",
+            request);
+    }
+
+    public Task<ApiResult<IReadOnlyList<ChannelResponseDto>>> GetByWorkspaceIdAsync(
+        Guid workspaceId)
+    {
+        return GetAsync<IReadOnlyList<ChannelResponseDto>>(
+            $"api/workspaces/{workspaceId}/channels");
+    }
+
+    public Task<ApiResult<ChannelResponseDto>> GetByIdAsync(
+        Guid channelId)
+    {
+        return GetAsync<ChannelResponseDto>(
+            $"api/channels/{channelId}");
+    }
+}
---
+++ backend/tools/ChatApp.SignalRTester/Clients/Channels/IChannelApiClient.cs
@@ -0,0 +1,18 @@
+﻿using ChatApp.Contracts.Channels.Requests;
+using ChatApp.Contracts.Channels.Responses;
+using ChatApp.SignalRTester.Models;
+
+namespace ChatApp.SignalRTester.Clients.Channels;
+
+public interface IChannelApiClient
+{
+    Task<ApiResult<ChannelResponseDto>> CreateAsync(
+        Guid workspaceId,
+        CreateChannelRequestDto request);
+
+    Task<ApiResult<IReadOnlyList<ChannelResponseDto>>> GetByWorkspaceIdAsync(
+        Guid workspaceId);
+
+    Task<ApiResult<ChannelResponseDto>> GetByIdAsync(
+        Guid channelId);
+}
