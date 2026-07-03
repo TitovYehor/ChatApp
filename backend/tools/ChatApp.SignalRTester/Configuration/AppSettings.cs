7c63adbcf9cc5e81f9a7677e0aa490d81937b397
Yehor Titov <tegor901@gmail.com>
2026-07-03 12:35:51 +0200

    add HubUrl setting in appsettings add HubUrl property in AppSettings class

--- backend/tools/ChatApp.SignalRTester/Configuration/AppSettings.cs
+++ backend/tools/ChatApp.SignalRTester/Configuration/AppSettings.cs
@@ -5,4 +5,6 @@ public class AppSettings
     public const string SectionName = "AppSettings";
 
     public string ApiBaseUrl { get; init; } = string.Empty;
+
+    public string HubUrl { get; set; } = string.Empty;
 }
