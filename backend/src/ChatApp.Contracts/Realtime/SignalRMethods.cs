3d4306a0669e202fd8d6d1f907ca7ed280bf870a
Yehor Titov <tegor901@gmail.com>
2026-07-04 11:24:10 +0200

    add SignalRMethods static class with possible methods in SignalR

---
+++ backend/src/ChatApp.Contracts/Realtime/SignalRMethods.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatApp.Contracts.Realtime;
+
+public static class SignalRMethods
+{
+    public const string JoinChannel =
+        nameof(JoinChannel);
+
+    public const string LeaveChannel =
+        nameof(LeaveChannel);
+}
