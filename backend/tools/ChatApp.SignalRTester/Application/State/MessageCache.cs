f0f4d4cd57534d0cbd6cf9eef477c68728e75a44
Yehor Titov <tegor901@gmail.com>
2026-07-06 13:57:21 +0200

    add MessageCache class for storing loaded messages

---
+++ backend/tools/ChatApp.SignalRTester/Application/State/MessageCache.cs
@@ -0,0 +1,58 @@
+﻿using ChatApp.Contracts.Messages.Responses;
+
+namespace ChatApp.SignalRTester.Application.State;
+
+public class MessageCache
+{
+    private readonly List<MessageResponseDto> _messages = [];
+
+    public IReadOnlyList<MessageResponseDto> Messages => _messages;
+
+    public void Replace(
+        IEnumerable<MessageResponseDto> messages)
+    {
+        _messages.Clear();
+
+        _messages.AddRange(messages);
+    }
+
+    public void Add(
+        MessageResponseDto message)
+    {
+        _messages.Add(message);
+    }
+
+    public bool Remove(
+        Guid messageId)
+    {
+        var message = _messages.FirstOrDefault(
+            x => x.Id == messageId);
+
+        if (message == null)
+        {
+            return false;
+        }
+
+        _messages.Remove(message);
+
+        return true;
+    }
+
+    public bool Update(
+        MessageResponseDto message)
+    {
+        var existing = _messages.FirstOrDefault(
+            x => x.Id == message.Id);
+
+        if (existing == null)
+        {
+            return false;
+        }
+
+        var index = _messages.IndexOf(existing);
+
+        _messages[index] = message;
+
+        return true;
+    }
+}
