30d1d63171c3995a33be937bb42b1693fe3de0ec
Yehor Titov <tegor901@gmail.com>
2026-07-07 12:33:33 +0200

    add RealtimeSession dependency to SignalRClient add use of RealtimeSession in all methods modify constructor in SignalRClient to look cleaner

--- backend/tools/ChatApp.SignalRTester/SignalR/SignalRClient.cs
+++ backend/tools/ChatApp.SignalRTester/SignalR/SignalRClient.cs
@@ -1,6 +1,7 @@
 ﻿using ChatApp.Contracts.Messages.Responses;
 using ChatApp.Contracts.Realtime;
 using ChatApp.SignalRTester.Configuration;
+using ChatApp.SignalRTester.Session;
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Options;
 
@@ -10,35 +11,23 @@ public class SignalRClient : ISignalRClient
 {
     private readonly HubConnection _connection;
 
+    private readonly RealtimeSession _realtimeSession;
+
     public SignalRClient(
-        IOptions<AppSettings> options)
+        IOptions<AppSettings> options,
+        RealtimeSession realtimeSession)
     {
+        _realtimeSession = realtimeSession;
+
         _connection =
             new HubConnectionBuilder()
                 .WithUrl(options.Value.HubUrl)
                 .WithAutomaticReconnect()
                 .Build();
 
-        _connection.On<MessageResponseDto>(
-            SignalREvents.MessageCreated,
-            dto =>
-            {
-                MessageCreated?.Invoke(dto);
-            });
+        RegisterLifecycleEvents();
 
-        _connection.On<MessageResponseDto>(
-            SignalREvents.MessageUpdated,
-            dto =>
-            {
-                MessageUpdated?.Invoke(dto);
-            });
-
-        _connection.On<Guid>(
-            SignalREvents.MessageDeleted,
-            id =>
-            {
-                MessageDeleted?.Invoke(id);
-            });
+        RegisterMessageEvents();
     }
 
     public bool IsConnected => _connection.State == HubConnectionState.Connected;
@@ -57,6 +46,8 @@ public class SignalRClient : ISignalRClient
         }
 
         await _connection.StartAsync();
+
+        _realtimeSession.Connected();
     }
 
     public async Task DisconnectAsync()
@@ -67,21 +58,73 @@ public class SignalRClient : ISignalRClient
         }
 
         await _connection.StopAsync();
+
+        _realtimeSession.Disconnected();
     }
 
-    public Task JoinChannelAsync(
+    public async Task JoinChannelAsync(
         Guid channelId)
     {
-        return _connection.InvokeAsync(
+        await _connection.InvokeAsync(
             SignalRMethods.JoinChannel,
             channelId);
+
+        _realtimeSession.JoinedChannel(channelId);
     }
 
-    public Task LeaveChannelAsync(
+    public async Task LeaveChannelAsync(
         Guid channelId)
     {
-        return _connection.InvokeAsync(
+        await _connection.InvokeAsync(
             SignalRMethods.LeaveChannel,
             channelId);
+
+        _realtimeSession.LeftChannel();
+    }
+
+    private void RegisterLifecycleEvents()
+    {
+        _connection.Closed += OnClosedAsync;
+    }
+
+    private Task OnClosedAsync(
+        Exception? exception)
+    {
+        _realtimeSession.Disconnected();
+
+        return Task.CompletedTask;
+    }
+
+    private void RegisterMessageEvents()
+    {
+        _connection.On<MessageResponseDto>(
+            SignalREvents.MessageCreated,
+            RaiseMessageCreated);
+
+        _connection.On<MessageResponseDto>(
+            SignalREvents.MessageUpdated,
+            RaiseMessageUpdated);
+
+        _connection.On<Guid>(
+            SignalREvents.MessageDeleted,
+            RaiseMessageDeleted);
+    }
+
+    private void RaiseMessageCreated(
+        MessageResponseDto message)
+    {
+        MessageCreated?.Invoke(message);
+    }
+
+    private void RaiseMessageUpdated(
+        MessageResponseDto message)
+    {
+        MessageUpdated?.Invoke(message);
+    }
+
+    private void RaiseMessageDeleted(
+        Guid guid)
+    {
+        MessageDeleted?.Invoke(guid);
     }
 }
