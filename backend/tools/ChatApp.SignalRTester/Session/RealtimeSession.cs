acb51450162633a2af843506bbcf72c7ca97aa28
Yehor Titov <tegor901@gmail.com>
2026-07-07 12:32:10 +0200

    add RealtimeSession alongside UserSession but for realtime functionality

---
+++ backend/tools/ChatApp.SignalRTester/Session/RealtimeSession.cs
@@ -0,0 +1,30 @@
+﻿namespace ChatApp.SignalRTester.Session;
+
+public class RealtimeSession
+{
+    public bool IsConnected { get; private set; }
+
+    public Guid? CurrentChannelId { get; private set; }
+
+    public void Connected()
+    {
+        IsConnected = true;
+    }
+
+    public void Disconnected()
+    {
+        IsConnected = false;
+        CurrentChannelId = null;
+    }
+
+    public void JoinedChannel(
+        Guid channelId)
+    {
+        CurrentChannelId = channelId;
+    }
+
+    public void LeftChannel()
+    {
+        CurrentChannelId = null;
+    }
+}
