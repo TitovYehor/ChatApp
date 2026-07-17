28d973333d17e3236fcb261982e3c511b8a437b8
Yehor Titov <tegor901@gmail.com>
2026-07-17 13:40:03 +0200

    add WorkspaceMemberResponseDto class duplicate WorkspaceRole enum into WorkspaceRoleDto for clear Domain and Contracts projects separation

---
+++ backend/src/ChatApp.Contracts/Workspaces/Enums/WorkspaceRoleDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatApp.Contracts.Workspaces.Enums;
+
+public enum WorkspaceRoleDto
+{
+    Owner = 1,
+
+    Admin = 2,
+
+    Member = 3
+}
---
+++ backend/src/ChatApp.Contracts/Workspaces/Responses/WorkspaceMemberResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using ChatApp.Contracts.Workspaces.Enums;
+
+namespace ChatApp.Contracts.Workspaces.Responses;
+
+public class WorkspaceMemberResponseDto
+{
+    public Guid UserId { get; set; }
+
+    public string Username { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public WorkspaceRoleDto Role { get; set; }
+
+    public DateTime JoinedAt { get; set; }
+}
