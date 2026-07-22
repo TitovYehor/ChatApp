7cd2bea159749073238f7142c04f00706e797452
Yehor Titov <tegor901@gmail.com>
2026-07-22 09:41:32 +0200

    add ChangeWorkspaceMemberRoleRequestDto

---
+++ backend/src/ChatApp.Contracts/Workspaces/Requests/ChangeWorkspaceMemberRoleRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using ChatApp.Contracts.Workspaces.Enums;
+
+namespace ChatApp.Contracts.Workspaces.Requests;
+
+public class ChangeWorkspaceMemberRoleRequestDto
+{
+    public string UsernameOrEmail { get; set; } = string.Empty;
+
+    public WorkspaceRoleDto Role { get; set; }
+}
