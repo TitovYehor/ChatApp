cfee7406901ed4e88192b1e74fe751ea4c0ea852
Yehor Titov <tegor901@gmail.com>
2026-07-26 09:41:19 +0200

    add IOnlineUserTracker interface and implement it

---
+++ backend/src/ChatApp.Application/Interfaces/IOnlineUserTracker.cs
@@ -0,0 +1,17 @@
+﻿namespace ChatApp.Application.Interfaces;
+
+public interface IOnlineUserTracker
+{
+    bool UserConnected(
+        Guid userId,
+        string connectionId);
+
+    bool UserDisconnected(
+        Guid userId,
+        string connectionId);
+
+    bool IsOnline(
+        Guid userId);
+
+    IReadOnlyCollection<Guid> GetOnlineUsers();
+}
---
+++ backend/src/ChatApp.RealTime/Services/OnlineUserTracker .cs
@@ -0,0 +1,74 @@
+﻿using ChatApp.Application.Interfaces;
+using System.Collections.Concurrent;
+
+namespace ChatApp.RealTime.Services;
+
+public class OnlineUserTracker : IOnlineUserTracker
+{
+    private readonly ConcurrentDictionary<Guid, HashSet<string>> _connections
+        = new();
+
+    private readonly object _syncRoot = new();
+
+    public bool UserConnected(
+        Guid userId,
+        string connectionId)
+    {
+        lock (_syncRoot)
+        {
+            if (!_connections.TryGetValue(
+                    userId,
+                    out var userConnections))
+            {
+                userConnections = new HashSet<string>();
+
+                _connections[userId] = userConnections;
+            }
+
+            var wasOffline = userConnections.Count == 0;
+
+            userConnections.Add(connectionId);
+
+            return wasOffline;
+        }
+    }
+
+    public bool UserDisconnected(
+        Guid userId,
+        string connectionId)
+    {
+        lock (_syncRoot)
+        {
+            if (!_connections.TryGetValue(
+                    userId,
+                    out var userConnections))
+            {
+                return false;
+            }
+
+            userConnections.Remove(connectionId);
+
+            if (userConnections.Count > 0)
+            {
+                return false;
+            }
+
+            _connections.TryRemove(
+                userId,
+                out _);
+
+            return true;
+        }
+    }
+
+    public bool IsOnline(
+        Guid userId)
+    {
+        return _connections.ContainsKey(userId);
+    }
+
+    public IReadOnlyCollection<Guid> GetOnlineUsers()
+    {
+        return _connections.Keys.ToList();
+    }
+}
