e373e212475ba74745687a3c1935984d124d0378
Yehor Titov <tegor901@gmail.com>
2026-07-26 09:27:31 +0200

    add mapping for CurrentUserRole to ToDto method in WorkspaceMappings

--- backend/src/ChatApp.Application/Mappings/WorkspaceMappings.cs
+++ backend/src/ChatApp.Application/Mappings/WorkspaceMappings.cs
@@ -1,19 +1,22 @@
 ﻿using ChatApp.Contracts.Workspaces.Responses;
 using ChatApp.Domain.Entities;
+using ChatApp.Domain.Enums;
 
 namespace ChatApp.Application.Mappings;
 
 public static class WorkspaceMappings
 {
     public static WorkspaceResponseDto ToDto(
-        this Workspace workspace)
+        this Workspace workspace,
+        WorkspaceRole role)
     {
         return new WorkspaceResponseDto
         {
             Id = workspace.Id,
             Name = workspace.Name,
             Description = workspace.Description,
-            CreatedAt = workspace.CreatedAt
+            CreatedAt = workspace.CreatedAt,
+            CurrentUserRole = role.ToDto()
         };
     }
 }
