38ebc98643e5ec1975443f3d14e7cc192799f940
Yehor Titov <tegor901@gmail.com>
2026-07-26 09:26:47 +0200

    add WorkspaceRoleMappings

---
+++ backend/src/ChatApp.Application/Mappings/WorkspaceRoleMappings.cs
@@ -0,0 +1,37 @@
+﻿using ChatApp.Contracts.Workspaces.Enums;
+using ChatApp.Domain.Enums;
+
+namespace ChatApp.Application.Mappings;
+
+public static class WorkspaceRoleMappings
+{
+    public static WorkspaceRoleDto ToDto(
+        this WorkspaceRole role)
+    {
+        return role switch
+        {
+            WorkspaceRole.Owner => WorkspaceRoleDto.Owner,
+            WorkspaceRole.Admin => WorkspaceRoleDto.Admin,
+            WorkspaceRole.Member => WorkspaceRoleDto.Member,
+            _ => throw new ArgumentOutOfRangeException(
+                nameof(role),
+                role,
+                "Unknown workspace role")
+        };
+    }
+
+    public static WorkspaceRole ToDomain(
+        this WorkspaceRoleDto role)
+    {
+        return role switch
+        {
+            WorkspaceRoleDto.Owner => WorkspaceRole.Owner,
+            WorkspaceRoleDto.Admin => WorkspaceRole.Admin,
+            WorkspaceRoleDto.Member => WorkspaceRole.Member,
+            _ => throw new ArgumentOutOfRangeException(
+                nameof(role),
+                role,
+                "Unknown workspace role")
+        };
+    }
+}
