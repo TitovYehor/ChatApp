2785ea1490ec267ae11294b0065a665502a13e23
Yehor Titov <tegor901@gmail.com>
2026-07-27 09:46:21 +0200

    add UserPresenceChangedResponseDto

---
+++ backend/src/ChatApp.Contracts/Realtime/UserPresenceChangedResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatApp.Contracts.Realtime;
+
+public class UserPresenceChangedResponseDto
+{
+    public Guid UserId { get; set; }
+
+    public string Username { get; set; } = string.Empty;
+
+    public bool IsOnline { get; set; }
+}
