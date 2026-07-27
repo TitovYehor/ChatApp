c20c33968f0aec4666a9ada43f222da0f156461f
Yehor Titov <tegor901@gmail.com>
2026-07-27 09:45:48 +0200

    add PresenceLookupResponseDto

---
+++ backend/src/ChatApp.Contracts/Workspaces/Responses/PresenceLookupResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatApp.Contracts.Workspaces.Responses;
+
+public class PresenceLookupResponseDto
+{
+    public Guid UserId { get; set; }
+
+    public string Username { get; set; } = string.Empty;
+
+    public IReadOnlyCollection<Guid> RecipientUserIds { get; set; }
+        = [];
+}
