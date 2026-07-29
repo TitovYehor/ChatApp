dbfcdc147568853f0a2bff6e6bc460c73459d588
Yehor Titov <tegor901@gmail.com>
2026-07-29 09:27:23 +0200

    add OnlineUsersCache

---
+++ backend/tools/ChatApp.SignalRTester/Application/State/OnlineUsersCache.cs
@@ -0,0 +1,32 @@
+﻿using ChatApp.Contracts.Realtime;
+
+namespace ChatApp.SignalRTester.Application.State;
+
+public class OnlineUsersCache
+{
+    private readonly Dictionary<Guid, UserPresenceChangedResponseDto> _users = [];
+
+    public void UserChanged(UserPresenceChangedResponseDto user)
+    {
+        if (user.IsOnline)
+        {
+            _users[user.UserId] = user;
+        }
+        else
+        {
+            _users.Remove(user.UserId);
+        }
+    }
+
+    public IReadOnlyCollection<UserPresenceChangedResponseDto> GetAll()
+    {
+        return _users.Values
+            .OrderBy(x => x.Username)
+            .ToList();
+    }
+
+    public void Clear()
+    {
+        _users.Clear();
+    }
+}
