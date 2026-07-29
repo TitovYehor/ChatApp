e2f8beb71025a03d0248ddba8abb959346f62af1
Yehor Titov <tegor901@gmail.com>
2026-07-29 09:28:41 +0200

    add OnlineUsersCache dependency to PresenceRealtimeHandler add call of OnlineUsersCache.UserChanged in OnUserPresenceChanged method

--- backend/tools/ChatApp.SignalRTester/Application/Realtime/PresenceRealtimeHandler.cs
+++ backend/tools/ChatApp.SignalRTester/Application/Realtime/PresenceRealtimeHandler.cs
@@ -1,22 +1,26 @@
 ﻿using ChatApp.Contracts.Realtime;
 using ChatApp.SignalRTester.Application.Startup;
+using ChatApp.SignalRTester.Application.State;
 using ChatApp.SignalRTester.SignalR;
 using ChatApp.SignalRTester.UI.Output;
 
 namespace ChatApp.SignalRTester.Application.Realtime;
 
-public class PresenceRealtimeHandler
-    : IApplicationInitializer
+public class PresenceRealtimeHandler : IApplicationInitializer
 {
     private readonly ISignalRClient _signalRClient;
+
+    private readonly OnlineUsersCache _onlineUsersCache;
 
     private readonly IConsoleOutput _consoleOutput;
 
     public PresenceRealtimeHandler(
         ISignalRClient signalRClient,
+        OnlineUsersCache onlineUsersCache,
         IConsoleOutput consoleOutput)
     {
         _signalRClient = signalRClient;
+        _onlineUsersCache = onlineUsersCache;
         _consoleOutput = consoleOutput;
     }
 
@@ -31,6 +35,8 @@ public class PresenceRealtimeHandler
     private void OnUserPresenceChanged(
         UserPresenceChangedResponseDto response)
     {
+        _onlineUsersCache.UserChanged(response);
+
         _consoleOutput.WriteUserPresenceChanged(
             response);
     }
