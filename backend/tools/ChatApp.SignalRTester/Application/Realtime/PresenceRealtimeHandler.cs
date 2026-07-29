979256896ddf8aa6f8d0a0dd2807c3d840bbefeb
Yehor Titov <tegor901@gmail.com>
2026-07-29 09:18:50 +0200

    add PresenceRealtimeHandler

---
+++ backend/tools/ChatApp.SignalRTester/Application/Realtime/PresenceRealtimeHandler.cs
@@ -0,0 +1,37 @@
+﻿using ChatApp.Contracts.Realtime;
+using ChatApp.SignalRTester.Application.Startup;
+using ChatApp.SignalRTester.SignalR;
+using ChatApp.SignalRTester.UI.Output;
+
+namespace ChatApp.SignalRTester.Application.Realtime;
+
+public class PresenceRealtimeHandler
+    : IApplicationInitializer
+{
+    private readonly ISignalRClient _signalRClient;
+
+    private readonly IConsoleOutput _consoleOutput;
+
+    public PresenceRealtimeHandler(
+        ISignalRClient signalRClient,
+        IConsoleOutput consoleOutput)
+    {
+        _signalRClient = signalRClient;
+        _consoleOutput = consoleOutput;
+    }
+
+    public Task InitializeAsync()
+    {
+        _signalRClient.UserPresenceChanged +=
+            OnUserPresenceChanged;
+
+        return Task.CompletedTask;
+    }
+
+    private void OnUserPresenceChanged(
+        UserPresenceChangedResponseDto response)
+    {
+        _consoleOutput.WriteUserPresenceChanged(
+            response);
+    }
+}
