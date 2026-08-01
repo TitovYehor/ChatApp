e1b39d8b031d64461777bd4bb7afbbedb3ab26b0
Yehor Titov <tegor901@gmail.com>
2026-08-01 09:23:54 +0200

    add UserTypingResponseDto

---
+++ backend/src/ChatApp.Contracts/Realtime/Responses/UserTypingResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatApp.Contracts.Realtime.Responses;
+
+public class UserTypingResponseDto
+{
+    public Guid UserId { get; set; }
+
+    public string Username { get; set; } = string.Empty;
+
+    public Guid ChannelId { get; set; }
+
+    public bool IsTyping { get; set; }
+}
