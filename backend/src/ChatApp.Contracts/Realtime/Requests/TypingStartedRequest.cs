634bf228ecfec5d925e2137e52f1406b92298788
Yehor Titov <tegor901@gmail.com>
2026-08-01 09:23:46 +0200

    add TypingStartedRequest and TypingStoppedRequest

---
+++ backend/src/ChatApp.Contracts/Realtime/Requests/TypingStartedRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace ChatApp.Contracts.Realtime.Requests;
+
+public class TypingStartedRequest
+{
+    public Guid ChannelId { get; set; }
+}
---
+++ backend/src/ChatApp.Contracts/Realtime/Requests/TypingStoppedRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace ChatApp.Contracts.Realtime.Requests;
+
+public class TypingStoppedRequest
+{
+    public Guid ChannelId { get; set; }
+}
