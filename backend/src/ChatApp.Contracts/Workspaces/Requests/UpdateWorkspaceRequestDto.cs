81358b2db68fcf89e12cbb083810faac38ef8ca5
Yehor Titov <tegor901@gmail.com>
2026-08-02 09:40:27 +0200

    add UpdateWorkspaceRequestDto

---
+++ backend/src/ChatApp.Contracts/Workspaces/Requests/UpdateWorkspaceRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatApp.Contracts.Workspaces.Requests;
+
+public class UpdateWorkspaceRequestDto
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+}
