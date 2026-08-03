a7888291ce134e4b72712cf913fb381a2467a8e3
Yehor Titov <tegor901@gmail.com>
2026-08-03 09:34:05 +0200

    add new ExplicitCascadeDeletes database migration

---
+++ backend/src/ChatApp.Infrastructure/Migrations/20260803072839_ExplicitCascadeDeletes.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExplicitCascadeDeletes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkspaceMembers_Users_UserId",
+                table: "WorkspaceMembers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkspaceMembers_Users_UserId",
+                table: "WorkspaceMembers",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkspaceMembers_Users_UserId",
+                table: "WorkspaceMembers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkspaceMembers_Users_UserId",
+                table: "WorkspaceMembers",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
