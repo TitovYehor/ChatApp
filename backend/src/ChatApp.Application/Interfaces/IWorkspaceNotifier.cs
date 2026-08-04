c5315e668369c15ac831158a06f515f8500899d0
Yehor Titov <tegor901@gmail.com>
2026-08-04 09:59:54 +0200

    add IWorkspaceNotifier interface and SignalRWorkspaceNotifier implementation

---
+++ backend/src/ChatApp.Application/Interfaces/IWorkspaceNotifier.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatApp.Application.Interfaces;
+
+public interface IWorkspaceNotifier
+{
+    Task WorkspaceDeletedAsync(
+        Guid workspaceId,
+        IReadOnlyCollection<Guid> memberIds);
+}
---
+++ backend/src/ChatApp.RealTime/Services/SignalRWorkspaceNotifier.cs
@@ -0,0 +1,32 @@
+﻿using ChatApp.Application.Interfaces;
+using ChatApp.Contracts.Realtime.SignalRNamings;
+using ChatApp.Contracts.Workspaces.Responses;
+using ChatApp.RealTime.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.RealTime.Services;
+
+public sealed class SignalRWorkspaceNotifier : IWorkspaceNotifier
+{
+    private readonly IHubContext<ChatHub> _hubContext;
+
+    public SignalRWorkspaceNotifier(
+        IHubContext<ChatHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public async Task WorkspaceDeletedAsync(
+        Guid workspaceId,
+        IReadOnlyCollection<Guid> memberIds)
+    {
+        await _hubContext.Clients
+            .Users(memberIds.Select(x => x.ToString()))
+            .SendAsync(
+                SignalREvents.WorkspaceDeleted,
+                new WorkspaceDeletedResponseDto
+                {
+                    WorkspaceId = workspaceId
+                });
+    }
+}
