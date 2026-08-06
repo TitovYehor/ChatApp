a3851ef8053229aaf25e6fd51e9c11e1b252c948
Yehor Titov <tegor901@gmail.com>
2026-08-06 09:21:20 +0200

    add WorkspaceUpdatedResponseDto

---
+++ backend/src/ChatApp.Contracts/Workspaces/Responses/WorkspaceUpdatedResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatApp.Contracts.Workspaces.Responses;
+
+public class WorkspaceUpdatedResponseDto
+{
+    public Guid WorkspaceId { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+}
