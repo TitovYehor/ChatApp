96f8755ea3c06704ef4bdb8f63b3e76d364b9b1f
Yehor Titov <tegor901@gmail.com>
2026-08-08 10:30:40 +0200

    add TypingRealtimeHandler for handling user typing events

---
+++ backend/tools/ChatApp.SignalRTester/Application/Realtime/TypingRealtimeHandler.cs
@@ -0,0 +1,47 @@
+﻿using ChatApp.Contracts.Realtime.Responses;
+using ChatApp.SignalRTester.Application.Startup;
+using ChatApp.SignalRTester.Session;
+using ChatApp.SignalRTester.SignalR;
+using ChatApp.SignalRTester.UI.Output;
+
+namespace ChatApp.SignalRTester.Application.Realtime;
+
+public class TypingRealtimeHandler : IApplicationInitializer
+{
+    private readonly ISignalRClient _signalRClient;
+    private readonly UserSession _userSession;
+    private readonly IConsoleOutput _consoleOutput;
+
+    public TypingRealtimeHandler(
+        ISignalRClient signalRClient,
+        UserSession userSession,
+        IConsoleOutput consoleOutput)
+    {
+        _signalRClient = signalRClient;
+        _userSession = userSession;
+        _consoleOutput = consoleOutput;
+    }
+
+    public Task InitializeAsync()
+    {
+        _signalRClient.UserTyping += OnTyping;
+
+        return Task.CompletedTask;
+    }
+
+    private void OnTyping(
+        UserTypingResponseDto response)
+    {
+        if (_userSession.CurrentChannel?.Id != response.ChannelId)
+        {
+            return;
+        }
+
+        if (response.UserId == _userSession.UserId)
+        {
+            return;
+        }
+
+        _consoleOutput.WriteUserTyping(response);
+    }
+}
