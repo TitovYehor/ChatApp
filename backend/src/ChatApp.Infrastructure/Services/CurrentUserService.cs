0195edd45f62b836102319f7e2a23f14ac09c09a
Yehor Titov <tegor901@gmail.com>
2026-08-14 09:38:00 +0200

    add CurrentUserServiceTests minor changes in GetUserId method code styling in CurrentUserService

--- backend/src/ChatApp.Infrastructure/Services/CurrentUserService.cs
+++ backend/src/ChatApp.Infrastructure/Services/CurrentUserService.cs
@@ -9,19 +9,17 @@ public class CurrentUserService : ICurrentUserService
 {
     private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public CurrentUserService(
-        IHttpContextAccessor httpContextAccessor)
+    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
     {
         _httpContextAccessor = httpContextAccessor;
     }
 
     public Guid GetUserId()
     {
-        var userIdClaim =
-            _httpContextAccessor
-                .HttpContext?
-                .User
-                .FindFirst(ClaimTypes.NameIdentifier);
+        var userIdClaim = _httpContextAccessor
+            .HttpContext?
+            .User
+            .FindFirst(ClaimTypes.NameIdentifier);
 
         if (userIdClaim == null)
         {
---
+++ backend/src/ChatApp.UnitTests/Services/CurrentUserServiceTests.cs
@@ -0,0 +1,93 @@
+﻿using ChatApp.Application.Exceptions;
+using ChatApp.Infrastructure.Services;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System.Security.Claims;
+
+namespace ChatApp.UnitTests.Services;
+
+public class CurrentUserServiceTests
+{
+    private readonly Mock<IHttpContextAccessor> _httpContextAccessorMock;
+
+    public CurrentUserServiceTests()
+    {
+        _httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+    }
+
+    [Fact]
+    public void GetUserId_WhenValidUserIdClaimExists_ReturnsUserId()
+    {
+        var userId = Guid.NewGuid();
+
+        SetUser(new ClaimsPrincipal(
+                    new ClaimsIdentity(
+                    [
+                        new Claim(
+                            ClaimTypes.NameIdentifier,
+                            userId.ToString())
+                    ],
+                    "TestAuth")));
+
+        var service = CreateService();
+
+        var result = service.GetUserId();
+
+        Assert.Equal(userId, result);
+    }
+
+    [Fact]
+    public void GetUserId_WhenUserIdClaimIsMissing_ThrowsUserNotAuthenticatedException()
+    {
+        SetUser(new ClaimsPrincipal(new ClaimsIdentity()));
+
+        var service = CreateService();
+
+        var exception = Assert.Throws<UserNotAuthenticatedException>(
+            () => service.GetUserId());
+
+        Assert.Equal(
+            "User is not authenticated",
+            exception.Message);
+    }
+
+    [Fact]
+    public void GetUserId_WhenUserIdClaimIsInvalid_ThrowsUserNotAuthenticatedException()
+    {
+        SetUser(new ClaimsPrincipal(
+                    new ClaimsIdentity(
+                    [
+                        new Claim(
+                            ClaimTypes.NameIdentifier,
+                            "not-a-guid")
+                    ],
+                    "TestAuth")));
+
+        var service = CreateService();
+
+        var exception = Assert.Throws<UserNotAuthenticatedException>(
+            () => service.GetUserId());
+
+        Assert.Equal(
+            "User is not authenticated",
+            exception.Message);
+    }
+
+    private CurrentUserService CreateService()
+    {
+        return new CurrentUserService(
+            _httpContextAccessorMock.Object);
+    }
+
+    private void SetUser(ClaimsPrincipal user)
+    {
+        var httpContext = new DefaultHttpContext
+        {
+            User = user
+        };
+
+        _httpContextAccessorMock
+            .Setup(x => x.HttpContext)
+            .Returns(httpContext);
+    }
+}
