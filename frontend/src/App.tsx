581e3ab165f026e7e8e223c8434369170ca2e0f1
Yehor Titov <tegor901@gmail.com>
2026-08-18 10:11:04 +0200

    add initial pages and their routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import ChatPage from './pages/ChatPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+
 function App() {
-  return (
-    <div>
-      <h1>ChatApp</h1>
-      <p>Frontend is running.</p>
-    </div>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/chat" element={<ChatPage />} />
+
+                <Route
+                    path="*"
+                    element={<Navigate to="/login" replace />}
+                />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
---
+++ frontend/src/pages/ChatPage.tsx
@@ -0,0 +1,10 @@
+function ChatPage() {
+    return (
+        <div>
+            <h1>ChatApp</h1>
+            <p>Chat page</p>
+        </div>
+    )
+}
+
+export default ChatPage
