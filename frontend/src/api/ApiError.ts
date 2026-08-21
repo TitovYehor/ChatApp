0bc827a5171566b76c27cb72fa36dc3b4689bd47
Yehor Titov <tegor901@gmail.com>
2026-08-21 10:01:43 +0200

    extend ApiError class with responseBody parameter

--- frontend/src/api/ApiError.ts
+++ frontend/src/api/ApiError.ts
@@ -1,13 +1,17 @@
 export class ApiError extends Error {
     public readonly status: number
 
+    public readonly responseBody: unknown
+
     constructor(
         status: number,
         message: string,
+        responseBody?: unknown,
     ) {
         super(message)
 
         this.name = 'ApiError'
         this.status = status
+        this.responseBody = responseBody
     }
 }
