245564897b87d2aa8f447b965d4a3c31c80efab3
Yehor Titov <tegor901@gmail.com>
2026-08-22 11:16:50 +0200

    add queryClient add QueryClientProvider to main.tsx file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClientProvider } from '@tanstack/react-query'
+
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './features/auth/AuthProvider'
+import { queryClient } from './lib/queryClient'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <AuthProvider>
-            <App />
+            <QueryClientProvider client={queryClient}>
+                <App />
+            </QueryClientProvider>
         </AuthProvider>
     </StrictMode>,
 )
