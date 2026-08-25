db9c1396f75a7d60646499991d3abbcd88c2eaf1
Yehor Titov <tegor901@gmail.com>
2026-08-25 09:58:29 +0200

    add AllowCredentials option to frontend policy in Program

--- backend/src/ChatApp.Api/Program.cs
+++ backend/src/ChatApp.Api/Program.cs
@@ -96,7 +96,8 @@ builder.Services.AddCors(options =>
             policy
                 .WithOrigins("http://localhost:5173")
                 .AllowAnyHeader()
-                .AllowAnyMethod();
+                .AllowAnyMethod()
+                .AllowCredentials();
         });
 });
 
