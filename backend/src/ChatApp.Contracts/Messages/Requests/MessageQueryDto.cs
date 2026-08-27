3bd2142efcb9d725fa84544652797c70c62c6a26
Yehor Titov <tegor901@gmail.com>
2026-08-27 10:15:04 +0200

    add data annotations to MessageQueryDto

--- backend/src/ChatApp.Contracts/Messages/Requests/MessageQueryDto.cs
+++ backend/src/ChatApp.Contracts/Messages/Requests/MessageQueryDto.cs
@@ -1,9 +1,13 @@
-﻿namespace ChatApp.Contracts.Messages.Requests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.Contracts.Messages.Requests;
 
 public class MessageQueryDto
 {
+    [Range(1, int.MaxValue)]
     public int PageNumber { get; set; } = 1;
 
+    [Range(1, 100)]
     public int PageSize { get; set; } = 50;
 
     public string? Search { get; set; }
