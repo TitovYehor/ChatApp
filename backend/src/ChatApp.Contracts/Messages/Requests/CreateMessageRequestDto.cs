17168cd47fd69cc37a2002c9cc632e6305af7ef2
Yehor Titov <tegor901@gmail.com>
2026-08-28 09:46:33 +0200

    add DataAnnotations for CreateMessageRequestDto

--- backend/src/ChatApp.Contracts/Messages/Requests/CreateMessageRequestDto.cs
+++ backend/src/ChatApp.Contracts/Messages/Requests/CreateMessageRequestDto.cs
@@ -1,6 +1,10 @@
-﻿namespace ChatApp.Contracts.Messages.Requests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.Contracts.Messages.Requests;
 
 public class CreateMessageRequestDto
 {
+    [Required]
+    [StringLength(2000)]
     public string Content { get; set; } = string.Empty;
 }
