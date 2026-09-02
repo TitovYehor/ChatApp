89f8ec4e88c9a72e1fc2a06218dca1e60c5bd307
Yehor Titov <tegor901@gmail.com>
2026-09-02 09:58:04 +0200

    add Create/UpdateChannelRequest types to channelTypes

--- frontend/src/types/channelTypes.ts
+++ frontend/src/types/channelTypes.ts
@@ -1,3 +1,11 @@
+export interface CreateChannelRequest {
+    name: string
+}
+
+export interface UpdateChannelRequest {
+    name: string
+}
+
 export interface ChannelResponse {
     id: string
     workspaceId: string
