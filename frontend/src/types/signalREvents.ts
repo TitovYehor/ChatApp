0d4850e14eb9f20381c35b498cccb2328bdda1ac
Yehor Titov <tegor901@gmail.com>
2026-09-11 09:20:41 +0200

    add signalREvents namings

---
+++ frontend/src/types/signalREvents.ts
@@ -0,0 +1,10 @@
+export const SignalREvents = {
+    MessageCreated: 'MessageCreated',
+    MessageUpdated: 'MessageUpdated',
+    MessageDeleted: 'MessageDeleted',
+    UserPresenceChanged: 'UserPresenceChanged',
+    OnlineUsersSnapshot: 'OnlineUsersSnapshot',
+    UserTyping: 'UserTyping',
+    WorkspaceDeleted: 'WorkspaceDeleted',
+    WorkspaceUpdated: 'WorkspaceUpdated',
+} as const
