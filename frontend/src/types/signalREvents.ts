c02551fab4f973bee4000afcd126a1c2d0c24a34
Yehor Titov <tegor901@gmail.com>
2026-09-13 09:31:50 +0200

    add WorkspaceMemberAdded to signalREvents

--- frontend/src/types/signalREvents.ts
+++ frontend/src/types/signalREvents.ts
@@ -2,9 +2,12 @@ export const SignalREvents = {
     MessageCreated: 'MessageCreated',
     MessageUpdated: 'MessageUpdated',
     MessageDeleted: 'MessageDeleted',
+
     UserPresenceChanged: 'UserPresenceChanged',
     OnlineUsersSnapshot: 'OnlineUsersSnapshot',
     UserTyping: 'UserTyping',
+
     WorkspaceDeleted: 'WorkspaceDeleted',
     WorkspaceUpdated: 'WorkspaceUpdated',
+    WorkspaceMemberAdded: 'WorkspaceMemberAdded',
 } as const
