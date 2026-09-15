c247fad80b7a8f8d5ecade51900444b18a06a904
Yehor Titov <tegor901@gmail.com>
2026-09-15 09:19:38 +0200

    add SignalRChannelNotifier as implementation of IChannelNotifier

---
+++ backend/src/ChatApp.RealTime/Notifications/SignalRChannelNotifier.cs
@@ -0,0 +1,29 @@
+using ChatApp.Application.Interfaces;
+using ChatApp.Contracts.Realtime.SignalRNamings;
+using ChatApp.Contracts.Channels.Responses;
+using ChatApp.RealTime.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.RealTime.Notifications;
+
+public sealed class SignalRChannelNotifier : IChannelNotifier
+{
+    private readonly IHubContext<ChatHub> _hubContext;
+
+    public SignalRChannelNotifier(
+        IHubContext<ChatHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public async Task ChannelCreatedAsync(
+        IReadOnlyCollection<Guid> memberIds,
+        ChannelResponseDto response)
+    {
+        await _hubContext.Clients
+            .Users(memberIds.Select(x => x.ToString()))
+            .SendAsync(
+                SignalREvents.ChannelCreated,
+                response);
+    }
+}
