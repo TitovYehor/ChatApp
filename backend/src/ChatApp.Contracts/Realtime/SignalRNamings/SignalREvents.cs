671260fe149c0b5c3f5cf21fcba28175d7eb022d
Yehor Titov <tegor901@gmail.com>
2026-09-16 09:11:46 +0200

    add ChannelUpdated event name to SignalREvents

--- backend/src/ChatApp.Contracts/Realtime/SignalRNamings/SignalREvents.cs
+++ backend/src/ChatApp.Contracts/Realtime/SignalRNamings/SignalREvents.cs
@@ -36,4 +36,7 @@ public static class SignalREvents
 
     public const string ChannelCreated =
         nameof(ChannelCreated);
+
+    public const string ChannelUpdated =
+        nameof(ChannelUpdated);
 }
