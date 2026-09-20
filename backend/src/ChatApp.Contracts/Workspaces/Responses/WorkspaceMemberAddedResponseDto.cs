22165f25a8e1bffa96c1023fc57bde8c4005addb
Yehor Titov <tegor901@gmail.com>
2026-09-20 09:30:24 +0200

    add new fields to WorkspaceMemberAddedResponseDto

--- backend/src/ChatApp.Contracts/Workspaces/Responses/WorkspaceMemberAddedResponseDto.cs
+++ backend/src/ChatApp.Contracts/Workspaces/Responses/WorkspaceMemberAddedResponseDto.cs
@@ -1,4 +1,3 @@
-using System;
 using ChatApp.Contracts.Workspaces.Enums;
 
 namespace ChatApp.Contracts.Workspaces.Responses;
@@ -11,9 +10,17 @@ public class WorkspaceMemberAddedResponseDto
 
     public string Description { get; set; } = string.Empty;
 
-    public WorkspaceRoleDto CurrentUserRole { get; set; } = WorkspaceRoleDto.Member;
+    public Guid UserId { get; set; }
+
+    public string Username { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public WorkspaceRoleDto Role { get; set; } = WorkspaceRoleDto.Member;
 
     public DateTime CreatedAt { get; set; }
+
+    public DateTime JoinedAt { get; set; }
 
     public string AddedByUsername { get; set; } = string.Empty;
 }
