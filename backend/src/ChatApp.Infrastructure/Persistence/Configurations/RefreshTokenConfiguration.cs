f0bcd036f2b3ba13280cc66e3a7bd8822067cdb8
Yehor Titov <tegor901@gmail.com>
2026-09-21 09:10:43 +0200

    add RefreshTokenConfiguration

---
+++ backend/src/ChatApp.Infrastructure/Persistence/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,28 @@
+using ChatApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChatApp.Infrastructure.Persistence.Configurations;
+
+public class RefreshTokenConfiguration
+    : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(
+        EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.TokenHash)
+            .IsRequired()
+            .HasMaxLength(64);
+
+        builder.HasIndex(x => x.TokenHash)
+            .IsUnique();
+
+        builder
+            .HasOne(x => x.User)
+            .WithMany(x => x.RefreshTokens)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
